143a41bdaa6e40583a619bcecea8ca31c6ef52de
msakuta <masahiro.sakuta@gmail.com>
2023-12-28 22:20:24 +0900

    Allow adding points by clicking

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,7 +1,8 @@
 use ::rustree::{Point, RTree};
 use eframe::{
     egui::{self, Context, Ui},
-    epaint::{pos2, Color32, Pos2, Rect},
+    emath::Align2,
+    epaint::{pos2, Color32, FontId, Pos2, Rect},
 };
 use rustree::BoundingBox;
 
@@ -49,13 +50,29 @@ impl RustreeApp {
     fn paint(&mut self, ui: &mut Ui) {
         let (_response, painter) = ui.allocate_painter(ui.available_size(), egui::Sense::click());
 
-        self.rtree.walk(&mut |_id, _level, bb| {
+        if _response.clicked() {
+            if let Some(pos) = _response.interact_pointer_pos() {
+                let screen_pos = (pos - self.offset.to_vec2()) / 10.;
+                let pt = Point {
+                    x: screen_pos.x as f64,
+                    y: screen_pos.y as f64,
+                };
+                self.rtree
+                    .insert_entry(pt, BoundingBox::from_minmax(pt, pt));
+            }
+        }
+
+        self.rtree.walk(&mut |id, level, bb| {
             if bb.get_area() == 0. {
-                painter.circle(
-                    pos2(bb.min.x as f32 * 10., bb.min.y as f32 * 10.) + self.offset.to_vec2(),
-                    3.,
-                    Color32::RED,
-                    (1., Color32::GREEN),
+                let pos =
+                    pos2(bb.min.x as f32 * 10., bb.min.y as f32 * 10.) + self.offset.to_vec2();
+                painter.circle(pos, 3., Color32::RED, (1., Color32::GREEN));
+                painter.text(
+                    pos,
+                    Align2::LEFT_CENTER,
+                    format!("{}, {}", id, level),
+                    FontId::new(16., egui::FontFamily::Proportional),
+                    Color32::BLACK,
                 );
             } else {
                 painter.rect_stroke(
